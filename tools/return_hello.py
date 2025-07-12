fb69007e8f551087d9a9ac26547019484309bd53
optimisuke <Naosuke.Ito@ibm.com>
2025-07-12 22:56:58 +0900

    エージェントとツール

---
+++ tools/return_hello.py
@@ -0,0 +1,13 @@
+from ibm_watsonx_orchestrate.agent_builder.tools import tool
+
+
+@tool
+def return_hello(name: str) -> str:
+    """
+    Returns a greeting with the provided name
+
+    :param name: The name to include in the greeting
+    :returns: A greeting string in the format "Hello [name]"
+    """
+
+    return "Hello " + name
